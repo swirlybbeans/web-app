503cef83e96465e11be6f1540186fc0f5c372931
swirlybbeans <140016106+swirlybbeans@users.noreply.github.com>
2024-05-06 10:32:48 -0500

    may-6

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const tasks = [];
+      const unordered_list = document.getElementById("taskList");
+      const input = document.querySelector("input");
+      const button = document.getElementById("add-btn");
+
+      function createDeleteButton(index) {
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-button");
+        deleteButton.addEventListener("click", () => {
+          tasks.splice(index, 1);
+          updateList();
+        });
+        return deleteButton;
+      }
+
+      function updateList() {
+        unordered_list.innerHTML = "";
+        for (let i = 0; i < tasks.length; i++) {
+          const li = document.createElement("li");
+          li.innerHTML = tasks[i];
+
+          const deleteButton = createDeleteButton(i);
+          li.appendChild(deleteButton);
+
+          unordered_list.appendChild(li);
+        }
+      }
+
+      button.addEventListener("click", () => {
+        const task = input.value.trim();
+        if (task !== "") {
+          tasks.push(task);
+          input.value = "";
+          updateList();
+        }
+      });
+
+      updateList();
